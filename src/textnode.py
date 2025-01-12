a96e4cf474d430513007612fbb3c59eee0eddc4d
Eric Haney <haneyeric@gmail.com>
2025-01-12 11:32:28 -0500

    done

--- src/textnode.py
+++ src/textnode.py
@@ -1,8 +1,9 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -23,3 +24,19 @@ class TextNode():
     def __repr__(self):
         text = "TextNode({}, {}, {})"
         return text.format(self.text, self.text_type.value, self.url)
+
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
